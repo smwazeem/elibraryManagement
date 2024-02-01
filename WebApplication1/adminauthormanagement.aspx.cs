185753f9aff1f7ed6d6e0f16253659e6209a3573
wazeemafro <mohamed@dxdy.tech>
2024-02-01 11:47:46 +0530

    first commit

---
+++ WebApplication1/adminauthormanagement.aspx.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class adminauthormanagement : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+        }
+        // Add click event
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (checkAuthorExists())
+            {
+                Response.Write("<script>alert('author_id is already exist')</script>");
+            }
+            else
+            {
+                addNewAuthor();
+            }
+        }
+        // Update click event
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (checkAuthorExists())
+            {
+                updateAuthor();
+            }
+            else
+            {
+                Response.Write("<script>alert('author_id is not exist')</script>");
+            }
+        }
+        // Delete click event
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (checkAuthorExists())
+            {
+                deleteAuthor();
+            }
+            else
+            {
+                Response.Write("<script>alert('author_id is not exist')</script>");
+            }
+        }
+        // Go click event
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            getAuthorById();
+        }
+
+        bool checkAuthorExists()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("select * from author_master_tbl where author_id = '" + TextBox1.Text.Trim() + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if(dt.Rows.Count > 0 )
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void addNewAuthor()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("insert into author_master_tbl(author_id,author_name) values (@author_id,@author_name)", con);
+                cmd.Parameters.AddWithValue("@author_id", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@author_name", TextBox2.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                Response.Write("<script>alert('author added')</script>");
+                clearForm();
+                GridView1.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        public void updateAuthor()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection (strcon);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update author_master_tbl set author_name=@author_name where author_id='" + TextBox1.Text.Trim() + "';", con);
+                cmd.Parameters.AddWithValue("@author_name", TextBox2.Text.Trim());
+
+                cmd.ExecuteNonQuery ();
+                con.Close();
+
+                Response.Write("<script>alert('updated')</script>");
+                clearForm();
+                GridView1.DataBind();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        public void deleteAuthor()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete from author_master_tbl where author_id='" + TextBox1.Text.Trim() + "';", con);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                Response.Write("<script>alert('deleted')</script>");
+                clearForm();
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        public void clearForm()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+        }
+
+        void getAuthorById()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("select * from author_master_tbl where author_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    TextBox2.Text = dt.Rows[0][1].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+    }
+}
---
+++ WebApplication1/adminmembermanagement.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class adminmembermanagement : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("delete from member_master_tbl where member_id='" + TextBox1.Text.Trim() + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                clearForm();
+                GridView1.DataBind();
+
+                Response.Write("<script>alert('Member is permanently deleted')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('"+ex.Message+"')</script>");
+
+            }
+        }
+
+        // GO button
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            GetMemberById();
+        }
+        // active button
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            updateMemberStatusById("active");
+        }
+        // pending button
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            updateMemberStatusById("pending");
+
+        }
+        // close button
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            updateMemberStatusById("deactive");
+
+        }
+
+        //user defined functions
+        void GetMemberById()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("select * from member_master_tbl where member_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        TextBox2.Text = dr[0].ToString();
+                        TextBox7.Text = dr[10].ToString();
+                        TextBox8.Text = dr[1].ToString();
+                        TextBox3.Text = dr[2].ToString();
+                        TextBox4.Text = dr[3].ToString();
+                        TextBox9.Text = dr[4].ToString();
+                        TextBox10.Text = dr[5].ToString();
+                        TextBox11.Text = dr[6].ToString();
+                        TextBox6.Text = dr[7].ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        void updateMemberStatusById(string status)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(strcon);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("update member_master_tbl set account_status='" + status + "' where member_id='" + TextBox1.Text.Trim() + "'", conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                GridView1.DataBind();
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+        }
+
+        void clearForm()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox7.Text = "";
+            TextBox8.Text = "";
+            TextBox3.Text = "";              
+            TextBox4.Text = "";
+            TextBox9.Text = "";
+            TextBox10.Text = "";
+            TextBox11.Text = "";
+            TextBox6.Text = "";
+        }
+    }
+}
